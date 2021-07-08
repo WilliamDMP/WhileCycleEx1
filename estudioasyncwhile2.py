242ac0d5ce4f80a1c9008c3fc71d13bbf9f0681e
William David Martinez Perez <wdmartinez89@gmail.com>
2021-07-07 19:10:30 -0500

    WhileEx2

---
+++ estudioasyncwhile2.py
@@ -0,0 +1,15 @@
+'''Ingresar numeros enteros por teclado e identificar cuantos de ellos son pares o impares, el programa finaliza al escribir 0 '''
+numeros = int(input('Ingrese numeros enteros para saber cuales son pares e impares '))
+numeropar = 0
+numeroimpar = 0
+while numeros > 0:
+    if numeros%2==0:
+        numeropar = numeropar+1
+    else:
+        numeroimpar = numeroimpar+1
+    numeros = int(input('Ingrese numeros enteros para saber cuales son pares e impares '))
+    
+print(f'la cantidad de numeros pares es {numeropar} y la cantidad de numeros impares es {numeroimpar}')
+
+        
+           
